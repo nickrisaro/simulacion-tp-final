ef52517d4b5a163b894d8af78851da3358a60329
Nick Risaro <nrisaro@gmail.com>
2020-09-04 09:39:33 -0300

    Hace privadas algunas funciones

--- Verosimilitud.py
+++ Verosimilitud.py
@@ -2,7 +2,7 @@ from scipy.stats import truncnorm
 
 import numpy as np
 
-def log_normal_truncada_desplazada(x, limite_inferior, limite_superior, media, desvio):
+def _log_normal_truncada_desplazada(x, limite_inferior, limite_superior, media, desvio):
     """
     Devuelve el log de la densidad de una normal truncada entre los límites indicados
     con la media y desvío recibidos
@@ -10,7 +10,7 @@ def log_normal_truncada_desplazada(x, limite_inferior, limite_superior, media, d
     a, b = (limite_inferior - media) / desvio, (limite_superior - media) / desvio
     return truncnorm.logpdf(x, a, b, loc = media, scale = desvio) # TODO La versión del paper devuelve -Inf si x == limite_inferior o x == limite_superior
 
-def log_prior_gamma_beta(x):
+def _log_prior_gamma_beta(x):
     """
     Calcula el log de la pdf para el parámetro gamma | beta
     Gamma | beta se comporta como una normal en el rango [1, 4] con media 2.5 y desvío estándar 1.5
@@ -19,9 +19,9 @@ def log_prior_gamma_beta(x):
     limite_inferior, limite_superior = 1, 4
     media, desvio = 2.5, 1.5
 
-    return log_normal_truncada_desplazada(x, limite_inferior, limite_superior, media, desvio)
+    return _log_normal_truncada_desplazada(x, limite_inferior, limite_superior, media, desvio)
 
-def log_prior_gamma(x):
+def _log_prior_gamma(x):
     """
     Calcula el log de la pdf para el parámetro gamma
     Gamma se comporta como una normal en el rango [3.4, 9.4] con media 6.4 y desvío estándar 1.5
@@ -30,9 +30,9 @@ def log_prior_gamma(x):
     limite_inferior, limite_superior = 3.4, 9.4
     media, desvio = 6.4, 1.5
 
-    return log_normal_truncada_desplazada(x, limite_inferior, limite_superior, media, desvio)
+    return _log_normal_truncada_desplazada(x, limite_inferior, limite_superior, media, desvio)
 
-def log_prior_phi(x):
+def _log_prior_phi(x):
     """
     Calcula el log de la pdf para el parámetro phi
     Phi es uniforme en el rango [0.01, 0.99]
@@ -41,7 +41,7 @@ def log_prior_phi(x):
         return 0
     return -np.Inf
 
-def log_prior_T0(x):
+def _log_prior_T0(x):
     """
     Calcula el log de la pdf para el parámetro T0
     T0 es uniforme en el rango [1, 60] (1 = 1/1/2020, 60 = 29/2/2020)
@@ -51,4 +51,4 @@ def log_prior_T0(x):
     return -np.Inf
 
 def log_prior(beta, gamma, T0, phi):
-    return log_prior_gamma_beta(beta/gamma) + log_prior_gamma(1/gamma) + log_prior_T0(T0) + log_prior_phi(phi)
+    return _log_prior_gamma_beta(beta/gamma) + _log_prior_gamma(1/gamma) + _log_prior_T0(T0) + _log_prior_phi(phi)
